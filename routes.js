b7c3f1d7bed543f8defd31f540770a043bcf43ab
mal-wood <mallorywood413@gmail.com>
2022-10-19 20:23:49 -0400

    updated

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/", function( res) {
   res.send("Welcome to the Webhooks API");
 });
 
