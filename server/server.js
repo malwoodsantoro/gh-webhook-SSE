b009126ab0ac02f535952721bc47a0e7d36f8ea2
mal-wood <mallorywood413@gmail.com>
2022-10-19 22:09:53 -0400

    clean up

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const Sequelize = require("sequelize");
-
 const sequelize = new Sequelize("sqlite://db.sqlite", { sync: true });
 
 sequelize
